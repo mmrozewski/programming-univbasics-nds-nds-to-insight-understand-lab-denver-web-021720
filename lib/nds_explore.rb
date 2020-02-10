2c9ce86dede0f37cb91c501a707583c138d28972
Mateusz Mrozewski <mmrozews@gmail.com>
2020-02-10 13:38:13 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -11,10 +11,121 @@ def pretty_print_nds(nds)
 end
 
 def print_first_directors_movie_titles
-  specific_stuff = directors_database[0][:movies]
+
+  first_director_movies = directors_database[0][:movies]
   row_index = 0 
- while row_index < specific_stuff.length do
-    puts (directors_database[0][:movies][row_index][:title])
+ while row_index < first_director_movies.length do
+    puts first_director_movies[row_index][:title]
     row_index += 1
+
   end
 end
+
+def gross_totals(nds)
+  
+  result = {}
+  name_index = 0
+  ww_gross_index = 0
+  ww_gross_total = 0
+  
+  while name_index < directors_database.length do
+    puts directors_database[name_index][:name]
+    
+    while ww_gross_index < directors_database[name_index].length do
+      
+      ww_gross_total += directors_database[name_index][:movies][ww_gross_index][:worldwide_gross]
+      
+      ww_gross_index += 1
+      
+    end
+    
+    result << {:directors_database[name_index][:name] => ww_gross_total }
+    
+    puts ww_gross_total
+    
+    name_index += 1 
+  end
+end 
+
+
+
+
+ 
+def movie_with_director_name(director_name, movie_data)
+  { 
+    :title => movie_data[:title],
+    :worldwide_gross => movie_data[:worldwide_gross],
+    :release_year => movie_data[:release_year],
+    :studio => movie_data[:studio],
+    :director_name => director_name
+  }
+end
+
+sample_coll = [{:title=>"The Godfather",
+     :studio=>"Paramount",
+     :worldwide_gross=>134966411,
+     :release_year=>1972},
+    {:title=>"Apocalypse Now",
+     :studio=>"MGM",
+     :worldwide_gross=>83471511,
+     :release_year=>1979},
+    {:title=>"Apocalypse Now Redux",
+     :studio=>"Miramax",
+     :worldwide_gross=>83471511,
+     :release_year=>2001},
+    {:title=>"Bram Stoker's Dracula",
+     :studio=>"Columbia",
+     :worldwide_gross=>82522790,
+     :release_year=>1992},
+    {:title=>"The Godfather Part III",
+     :studio=>"Paramount",
+     :worldwide_gross=>66666062,
+     :release_year=>1990},
+    {:title=>"Jack",
+     :studio=>"Buena Vista",
+     :worldwide_gross=>58620973,
+     :release_year=>1996}]
+
+def movies_with_director_key(name, movies_collection)
+  result = []
+  index = 0 
+  
+  while index < movies_collection.length do
+    result << movie_with_director_name(name, movies_collection[index])
+    index += 1 
+  end
+  result
+end
+
+
+
+
+ 
+result = {}
+studio_index = 0
+gross_index = 0
+studio_gross = 0
+  
+  while studio_index < sample_coll.length do
+    while gross_index < sample_coll.length do
+      if sample_coll[studio_index][:studio] == sample_coll[gross_index][:studio]
+        
+        result[sample_coll[studio_index][:studio]] = sample_coll[studio_index][:worldwide_gross] + sample_coll[gross_index][:worldwide_gross]
+        
+        gross_index += 1
+      else
+        gross_index += 1
+      end
+    end
+    
+    
+    
+    studio_index += 1 
+    
+    result[sample_coll[studio_index][:studio]] = [sample_coll[studio_index][:worldwide_gross]]
+  end 
+ 
+puts result
+
+
+
